cc421c111911f8e6105496a540e19ff6f24f2c19
Marcelo Marques <marcelo-mls@hotmail.com>
2022-07-07 20:25:26 -0300

    Requisito 5 concluido

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class ProductsList extends React.Component {
     state = {
       inputSearch: '',
       categoriesList: [],
+      productsList: [],
     }
 
     componentDidMount() {
@@ -26,8 +27,16 @@ class ProductsList extends React.Component {
       });
     }
 
+    callgetProductsFromCategoryAndQuery = async () => {
+      const { inputSearch } = this.state;
+      const products = await getProductsFromCategoryAndQuery(undefined, inputSearch);
+      this.setState({
+        productsList: products.results,
+      });
+    }
+
     render() {
-      const { inputSearch, categoriesList } = this.state;
+      const { inputSearch, categoriesList, productsList } = this.state;
       return (
         <div>
           <label htmlFor="inputSearch">
@@ -35,16 +44,24 @@ class ProductsList extends React.Component {
               type="text"
               name="inputSearch"
               id="inputSearch"
+              data-testid="query-input"
+              value={ inputSearch }
               onChange={ this.handleChange }
             />
           </label>
-          {inputSearch.length === 0
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.callgetProductsFromCategoryAndQuery }
+          >
+            Pesquisar
+          </button>
+          { inputSearch.length === 0
           && (
             <p
               data-testid="home-initial-message"
             >
               Digite algum termo de pesquisa ou escolha uma categoria.
-
             </p>)}
           <Link to="/cart" data-testid="shopping-cart-button">carrinho</Link>
           { categoriesList.map((category) => (
@@ -56,8 +73,18 @@ class ProductsList extends React.Component {
                 id="categoryRadio"
               />
               {category.name}
-            </label>)) }
-
+            </label>
+          )) }
+          { productsList.map((product) => (
+            <div
+              key={ product.id }
+              data-testid="product"
+            >
+              <h3>{ product.title}</h3>
+              <img src={ product.thumbnail } alt={ product.title } />
+              <p>{product.price}</p>
+            </div>
+          )) }
         </div>
       );
     }
